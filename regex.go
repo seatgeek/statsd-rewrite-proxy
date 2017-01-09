828dbd9127bbb587bfe0d55194c87f1db5f772cf
Christian Winther <cw@bownty.com>
2017-01-09 15:04:56 +0100

    add litterals support

--- regex.go
+++ regex.go
@@ -34,11 +34,10 @@ func buildRegexp(rule string) *regexp.Regexp {
 		// if the chunk contains markers, make it into a pattern match
 		if chunk[:1] == "{" {
 			chunk = fmt.Sprintf(`(?P<%s>[^\.]+)`, chunk[1:len(chunk)-1])
-		}
-
-		// if the chunk is a star, it means the rest
-		if chunk[:1] == "*" {
+		} else if chunk[:1] == "*" { // Stars will just glob anything
 			chunk = `.+?`
+		} else { // litterals will be, well, litterals and just escape for safe regexp processing
+			chunk = regexp.QuoteMeta(chunk)
 		}
 
 		regRule = append(regRule, chunk)
