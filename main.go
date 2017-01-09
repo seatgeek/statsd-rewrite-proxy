eb53ac72e67898874ed5c3e765ff9ca90913e14e
Christian Winther <cw@bownty.com>
2017-01-09 15:19:03 +0100

    change dd key to ensure uniqueness going forward

--- main.go
+++ main.go
@@ -78,9 +78,9 @@ func main() {
 
 func createRules() {
 	// fabio rewrites
-	rules = append(rules, NewRule("fabio.http.status.{code}", "fabio.http.response_code"))
-	rules = append(rules, NewRule("fabio.{service}.{host}.{path}.{upstream}.{dimension}", "fabio.service.requests.{dimension}"))
-	rules = append(rules, NewRule("fabio.{service}.{host}.{upstream}", "fabio.service.requests"))
+	rules = append(rules, NewRule("fabio.http.status.{code}", "b.fabio.http.response_code"))
+	rules = append(rules, NewRule("fabio.{service}.{host}.{path}.{upstream}.{dimension}", "b.fabio.service.requests.{dimension}"))
+	rules = append(rules, NewRule("fabio.{service}.{host}.{upstream}", "b.fabio.service.requests"))
 
 	// nomad rewrites
 	rules = append(rules, NewRule("nomad.client.uptime.*", "nomad.client.uptime"))
